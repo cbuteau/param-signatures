f7272011c13a40e6d23fccb49ce0e247a0bc4318
Christopher Buteau <cbuteau@gmail.com>
2016-03-19 21:27:41 -0400

    Integrating with Travis CI

---
+++ report.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# run by typing
+# bash report.sh
+
+# echo "Hello World!"
+
+# this uses node-inspector on the spec assigned.
+#node-debug /c/Users/Christopher/AppData/Roaming/npm/node_modules/jasmine/bin/jasmine.js ./spec/typeCodeSpec.js
+
+node jasmine-runner.js
